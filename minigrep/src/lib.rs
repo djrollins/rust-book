145c72c7834979a12322cafbbab77b3ce42c223d
Daniel J. Rollins <daniel@djrollins.com>
2019-03-17 18:54:08 +0000

    Use args iterator directly in minigrep

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -9,13 +9,22 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        args.next(); // skip over program name
+
+        let query = match args.next() {
+            Some(query) => query,
+            None => return Err("No query string specified."),
+        };
+
+        let filename = match args.next() {
+            Some(filename) => filename,
+            None => return Err("No filename specified."),
+        };
 
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
