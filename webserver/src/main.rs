d6c0be23f0b3ce79c88a6bfbcde74456a26ccd1f
Daniel J. Rollins <daniel@djrollins.com>
2019-03-24 11:43:13 +0000

    Add sleep endpoint

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,3 +1,5 @@
+use std::thread;
+use std::time::Duration;
 use std::io::prelude::*;
 use std::fs;
 use std::net::{
@@ -21,9 +23,13 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "slept.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
